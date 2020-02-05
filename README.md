4f71c1f75eb9fc602b692d0557fb868b9e3aca80
John Davis <johnedavis624@gmail.com>
2020-02-04 19:27:50 -0600

    5 day forecast

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JEDavis624_Server_Side_API
 Server Side API's
+
+As a traveler
+I want to see the weather outlook for multiple cities
+so that I can plan a trip accordingly
+
+Type in the city and retrieve its current weather along with the 5-day forecast.
--- assets/app.js
+++ assets/app.js
@@ -17,6 +17,7 @@ $("#find-city").on("click", function(event) {
         console.log(queryURLcurrent);
         console.log(response);
         
+        $("#img").text(response.weather[0].icon);
         $("#city").html("<h2>" + response.name + now + "</h2>");
         $("#temp").text("Temperature: " + response.main.temp + " °F");
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
@@ -45,6 +46,27 @@ $("#find-city").on("click", function(event) {
         }).then(function(response) {
           console.log(queryURL5day);
           console.log(response);
+
+          // $("#dateDay1").html("<p>" + response.list[0].sys.dt_txt + "</p>");
+          $("#tempDay1").text("Temperature: " + response.list[0].main.temp + " °F");
+          $("#humidityDay1").text("Humidity: " + response.list[0].main.humidity + "%");
+          
+
+          $("#tempDay2").text("Temperature: " + response.list[7].main.temp + " °F");
+          $("#humidityDay2").text("Humidity: " + response.list[7].main.humidity + "%");
+          
+
+          $("#tempDay3").text("Temperature: " + response.list[15].main.temp + " °F");
+          $("#humidityDay3").text("Humidity: " + response.list[15].main.humidity + "%");
+          
+
+          $("#tempDay4").text("Temperature: " + response.list[23].main.temp + " °F");
+          $("#humidityDay4").text("Humidity: " + response.list[23].main.humidity + "%");
+          
+
+          $("#tempDay5").text("Temperature: " + response.list[31].main.temp + " °F");
+          $("#humidityDay5").text("Humidity: " + response.list[31].main.humidity + "%");
+          
         
       });
 
@@ -52,3 +74,5 @@ $("#find-city").on("click", function(event) {
     })
   });
 })
+
+
